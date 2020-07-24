7b42a518e4192caf1a74f533d343e15c94013d1e
jonathankamau <kamaujonathan@gmail.com>
2020-07-24 20:38:44 +0300

    [Chore] add preliminary render functions for the views

---
+++ app/views.py
@@ -0,0 +1,13 @@
+from django.shortcuts import render
+
+def register(request):
+
+    return render(request, 'register.html')
+
+def login(request):
+
+    return render(request, 'register.html')
+
+def dashboard(request):
+
+    return render(request, 'register.html')
