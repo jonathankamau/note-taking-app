702e310b392a328dd50dbcbe65aa9327fe70f0b4
jonathankamau <kamaujonathan@gmail.com>
2020-07-25 18:28:22 +0300

    [Feature] update register view method

--- app/views.py
+++ app/views.py
@@ -1,8 +1,25 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from app.models import User, Note, MeetingCategory
+from django.contrib.auth import login, authenticate
+from app.forms import RegistrationForm
 
 def register(request):
 
-    return render(request, 'register.html')
+    if request.method == 'POST':
+        form_data = RegistrationForm(request.POST)
+
+        if form_data.is_valid():
+            form_data.save()
+            password = form_data.cleaned_data.get('password')
+
+            password = form_data.cleaned_data.get('password')
+            # user = authenticate(username=username)
+
+            return redirect('login')
+    else:
+        form_data = RegistrationForm()
+
+    return render(request, 'register.html', {'form': form_data})
 
 def login(request):
 
