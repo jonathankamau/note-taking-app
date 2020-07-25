ca076bc706f99e4d568c90055696cd246617f060
jonathankamau <kamaujonathan@gmail.com>
2020-07-25 19:35:28 +0300

    [Feature] add a url redirect for the login page

--- app/urls.py
+++ app/urls.py
@@ -21,4 +21,5 @@ from app import views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('register/', views.register, name='register'),
+    path('login/', views.login, name='login'),
 ]
