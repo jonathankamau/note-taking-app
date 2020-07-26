bfbd8e674f29753db2e8fad58dab78310cedf8bf
jonathankamau <kamaujonathan@gmail.com>
2020-07-26 15:39:36 +0300

    [Feature] add a NotesForm class to create form

--- app/forms.py
+++ app/forms.py
@@ -1,6 +1,6 @@
 from django import forms
 from django.contrib.auth.forms import UserCreationForm
-from app.models import UserProfile
+from app.models import UserProfile, Note, MeetingCategory
 
 class RegistrationForm(UserCreationForm):
     username = forms.CharField(label='User Name', max_length=100, required=True)
@@ -21,4 +21,18 @@ class RegistrationForm(UserCreationForm):
         if commit:
             user.save()
         return user
+
+class NotesForm(forms.ModelForm):
+    title = forms.CharField(label='Title', max_length=100, required=True)
+    org_name = forms.CharField(label='Organization Name', max_length=100, required=True)
+    purpose = forms.CharField(label='Purpose of the meeting', max_length=100, required=True)
+    content = forms.CharField(label='Add your Note here', max_length=500, required=True, widget=forms.Textarea)
+    total_attendance = forms.IntegerField(label='Enter the total number of participaants', required=True)
+
+    categories = [(prop['name'], prop['name']) for prop in MeetingCategory.objects.values('name').distinct()]
+    meeting_category = forms.ChoiceField(label='Select meeting category', required=True, choices=categories)
+
+    class Meta:
+        model = Note
+        fields = ('title', 'org_name', 'purpose', 'content', 'total_attendance', 'meeting_category')
 
