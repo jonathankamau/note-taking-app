85c968273d0e8bf2c02fb1c1985884dcd1ec8c3d
jonathankamau <kamaujonathan@gmail.com>
2020-07-30 20:23:13 +0300

    [Chore] add a newline for the account urls

--- app/modules/account/urls.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from django.conf.urls import url
-from app.modules.account.views import RegisterView, LoginView
-
-
-urlpatterns = [
-    url(r'^accounts/register/$', RegisterView.as_view(), name='register'),
-    url(r'^accounts/login/$', LoginView.as_view(), name='login'),
-]
