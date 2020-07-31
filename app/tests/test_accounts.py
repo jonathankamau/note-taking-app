a675eb93ab74e2030560d77786fdc3240694c5d1
jonathankamau <kamaujonathan@gmail.com>
2020-07-31 13:45:00 +0300

    [Chore] add preliminary tests for account

---
+++ app/tests/test_accounts.py
@@ -0,0 +1,14 @@
+from app.tests.test_base import BaseTestClass, RequestFactory, UserProfile
+from app.components.account.views import RegisterView
+
+class TestSignUp(BaseTestClass):
+
+    def test_user_is_registered(self):
+
+        self.assertEqual(self.register.status_code, 200)
+
+class TestLogin(BaseTestClass):
+
+    def test_user_logged_in(self):
+
+        self.assertEqual(self.user_login.status_code, 200)
---
+++ app/tests/test_base.py
@@ -0,0 +1,42 @@
+import json
+from django.test import Client, TestCase, RequestFactory
+from app.models import UserProfile
+from django.urls import reverse
+
+class BaseTestClass(TestCase):
+
+    def setUp(self):
+        self.client = Client()
+        fixtures = ['/app/data/categories.json',]
+
+        user_registration_details = {'first_name': 'Jay',
+                                    'last_name': 'Kline',
+                                    'username': 'kline',
+                                    'email': 'jay.kline@email.com',
+                                    'password': 'jaykline2020'
+                                }
+
+        registration_wrong_creds = {'first_name': 'Jay',
+                                    'last_name': 'Kline',
+                                    'username': 'kline',
+                                    'email': 'jay.kline',
+                                    'password': 'j'
+                                }
+        user_login_details = {
+            'username': 'kline',
+            'password': 'jaykline2020'
+
+        }
+
+
+        self.register = self.client.post(
+            reverse('register'), user_registration_details)
+        
+        
+        self.reg_wrong_creds = self.client.post(
+            reverse('register'), registration_wrong_creds)
+        
+        self.user_login = self.client.post(
+            reverse('login'), user_login_details)
+        
+
